0b0db7478b84212d63ade5b6993f5db83a4f65bd
Isaac Yriarte <isaacyagi@gmail.com>
2021-06-29 11:02:37 -0500

    Feat: Implementa search en giphy mediante rounting

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import React, { useState } from 'react';
 import  ListOfGifs from './components/ListOfGifs';
 import ApikeyManager from './components/ApikeyManager';
 import { Link, Route } from "wouter";
+import { useHistory } from "react-router-dom";
+
 function App() {
   const [keyword, setKeyword] = useState('Panda');
+  const history = useHistory();
+  //console.log(history)
+
+  const handlerSearchClick = () => history.push(`/gif/${keyword}`)
   return (
     <div className="App">
       <section className="App-content">
         <h1>App</h1>
+        <label>Search keyword</label>
+        <input value={keyword} onChange={(event)=> setKeyword(event.target.value)}></input>
+        <button onClick={handlerSearchClick}>Search</button>
         <ApikeyManager/>
         <Link to="/gif/panda">Gifs de pandas</Link>
         <Link to="/gif/perro">Gifs de perros</Link>
