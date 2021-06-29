d0805a2027d2f2bc5df0571358b4a98b92379495
Isaac Yriarte <isaacyagi@gmail.com>
2021-06-29 15:29:27 -0500

    Fix: Remueve funcion no usada

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -4,9 +4,6 @@ import './Gif.css'
 
 export default function Gif({title, id, url}){
 
-    const handlerClickDetailGif = () =>{
-
-    }
     return <Link href={`/gifdetail/${id}`} className="Gif">
               <h4>{title}</h4>
               <p>Id:<small>{id}</small></p>
