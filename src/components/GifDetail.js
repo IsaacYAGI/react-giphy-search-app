1ab31744db3542d1775f0216b0f91aa94c72d4c0
Isaac Yriarte <isaacyagi@gmail.com>
2021-06-29 13:28:25 -0500

    Feat: Se mapean los datos del detalle del gif

--- src/components/GifDetail.js
+++ src/components/GifDetail.js
@@ -1,9 +1,15 @@
 import { React, useState, useEffect } from 'react';
 
-export default function GifDetail({title, id, url}){
+export default function GifDetail({title, id, url, directUrl, rating}){
+    
     return (
         <div>
             Detail gif
+            <div>Id: {id}</div>
+            <div>Title: {title}</div>
+            <div>DirectUrl: <a href={directUrl} target="_blank">{directUrl}</a></div>
+            <div>Rating: {rating}</div>
+            <img src={url}/>
         </div>
     )
 }
--- src/pages/GifDetailPage.js
+++ src/pages/GifDetailPage.js
@@ -1,13 +1,38 @@
 import { React, useState, useEffect } from 'react';
 import GifDetail from '../components/GifDetail';
+import { GetGifById } from '../services/getGifDetail';
+
 
 export default function GifDetailPage({params}){
     const {id} = params;
+    const [gif, setGif] = useState({});
     console.log("GifDetailPage:", id)
+    const [loading, setLoading] = useState(false);
+    useEffect(()=>{
+        setLoading(true);
+        
+        GetGifById(id).then(gif => {
+            setLoading(false);
+            setGif(gif)
+            console.log(gif)
+        });
+        
+    }, [])
+    
+
+    if (loading) return <span>Cargando...</span>
+
+
     return (
-        <div>
+        gif ? (<div>
             <h1>Gif Detail</h1>
-            <GifDetail></GifDetail>
-        </div>
+            <GifDetail
+                title={gif.title}
+                id={gif.id}
+                url={gif.url}
+                directUrl={gif.directUrl}
+                rating={gif.rating}
+            />
+        </div>): (<div>No data to display</div> )
     )
 }
